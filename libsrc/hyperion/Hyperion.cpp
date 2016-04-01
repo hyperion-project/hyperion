459de47f893b3518809de6c922662c34c1e244d3
AEtHeLsYn <aethelsyn@hotmail.com>
2016-04-01 11:26:35 +0200

    Update color effect order and don't correct twice

--- libsrc/hyperion/Hyperion.cpp
+++ libsrc/hyperion/Hyperion.cpp
@@ -853,10 +853,11 @@ void Hyperion::update()
 	const PriorityMuxer::InputInfo & priorityInfo  = _muxer.getInputInfo(priority);
 
 	// Apply the correction and the transform to each led and color-channel
-	std::vector<ColorRgb> correctedColors = _raw2ledCorrection->applyCorrection(priorityInfo.ledColors);
-	std::vector<ColorRgb> adjustedColors = _raw2ledAdjustment->applyAdjustment(correctedColors);
-	std::vector<ColorRgb> temperatureColors = _raw2ledTemperature->applyCorrection(adjustedColors);
-	std::vector<ColorRgb> ledColors =_raw2ledTransform->applyTransform(temperatureColors);
+	// Avoid applying correction, the same task is performed by adjustment
+	// std::vector<ColorRgb> correctedColors = _raw2ledCorrection->applyCorrection(priorityInfo.ledColors);
+	std::vector<ColorRgb> adjustedColors = _raw2ledAdjustment->applyAdjustment(priorityInfo.ledColors);
+	std::vector<ColorRgb> transformColors =_raw2ledTransform->applyTransform(adjustedColors);
+	std::vector<ColorRgb> ledColors = _raw2ledTemperature->applyCorrection(transformColors);
 	const std::vector<Led>& leds = _ledString.leds();
 	int i = 0;
 	for (ColorRgb& color : ledColors)
