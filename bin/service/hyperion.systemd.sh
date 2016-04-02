006f49d033666a4453c258e45a992b390c964c13
penfold42 <penfold42@users.noreply.github.com>
2016-04-02 16:47:10 +1100

    Removed -HUP so the default -TERM signal is sent instead. - hyperiond only listens for TERM and INT. HUP is often used to get an exe to reread its config
    Changed pgrep to add '-x' so it wont partial match on the exe name.
    - I have multiple instances with multiple hyperiond-instance1 names
    - this ensures the service script only kills the right process

--- bin/service/hyperion.systemd.sh
+++ bin/service/hyperion.systemd.sh
@@ -7,7 +7,7 @@ User=root
 Group=root
 UMask=007
 ExecStart=/opt/hyperion/bin/hyperiond /etc/hyperion.config.json
-ExecReload=/bin/kill -HUP $MAINPID
+ExecReload=/bin/kill $MAINPID
 Restart=on-failure
 TimeoutStopSec=10
  
