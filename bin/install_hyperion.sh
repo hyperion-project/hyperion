5670f8135f0dc6ab8622819f933c3f6f8aadbe2b
brindosch <edeltraud70@gmx.de>
2016-06-10 10:45:13 +0200

    update

--- bin/install_hyperion.sh
+++ bin/install_hyperion.sh
@@ -132,6 +132,7 @@ fi
  
 # compatibility layer to move old configs to new config dir
 if [ -f "/opt/hyperion" ]; then
+	echo '---> Old installation found, move configs to /etc/hyperion/ and move hyperion to /usr/share/hyperion/'
 	mv /opt/hyperion/config/*.json /etc/hyperion 2>/dev/null 
 	sed -i "s|/opt/hyperion/effects|/usr/share/hyperion/effects|g" /etc/hyperion/*.json
 		if [ $USE_INITCTL -eq 1 ]; then
